0b2a0cd50278d6e930c854cf3dfb0f6f3ed56b48
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2025-10-12 01:17:41 +0200

    Updated file header files to reflect copyright

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-// * Original work: Copyright (c) Veljko Vuckovic (Refloow.com) All rights reserved.
+/* Refloow Video Editor
+ * Copyright (C) 2025  Veljko Vuckovic (Refloow) <legal@refloow.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 
 // main.js
 
@@ -235,4 +251,19 @@ ipcMain.handle('render-video', async (event, edl) => {
     });
 });
 
-// * Original work: Copyright (c) Veljko Vuckovic (Refloow.com) All rights reserved.
+/* Refloow Video Editor
+ * Copyright (C) 2025  Veljko Vuckovic (Refloow) <legal@refloow.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
