ce491e1dd407497e6c0b6f902585b665de1bd538
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2025-10-12 01:18:03 +0200

    Update copyright information in preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,19 @@
-// * Original work: Copyright (c) Veljko Vuckovic (Refloow.com) All rights reserved.
+/* Refloow Video Editor
+ * Copyright (C) 2025  Veljko Vuckovic (Refloow) <legal@refloow.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 const { contextBridge, ipcRenderer } = require('electron');
 
@@ -15,5 +30,19 @@ contextBridge.exposeInMainWorld('electronAPI', {
     }
 });
 
-// * Original work: Copyright (c) Veljko Vuckovic (Refloow.com) All rights reserved.
-
+/* Refloow Video Editor
+ * Copyright (C) 2025  Veljko Vuckovic (Refloow) <legal@refloow.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
